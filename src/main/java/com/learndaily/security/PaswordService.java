24f5e23334eddd58883a53c9c80b865ed4d8f1e3
karnan <sooriyakumarkarnan@gmail.com>
2022-04-08 09:42:49 +0530

    first commit

---
+++ src/main/java/com/learndaily/security/PaswordService.java
@@ -0,0 +1,27 @@
+package com.learndaily.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PaswordService {
+
+	public static String encode(String txt) {
+		Encoder encoder = Base64.getEncoder();
+		return encoder.encodeToString(txt.getBytes());
+	}
+	public static String decode(String entxt) {
+		Decoder decoder = Base64.getDecoder();
+		byte[] b = decoder.decode(entxt);
+		return new String(b);
+		
+	}
+	
+	public static void main(String[] args) {
+		String encode = PaswordService.encode("asokit");
+		System.out.println(encode);
+		
+		String text = PaswordService.decode(encode);
+		System.out.println(text);
+	}
+}
